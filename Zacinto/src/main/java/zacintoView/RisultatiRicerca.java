6280b4a03d36015972777f28223ec18c9b3b3ae3
FA <FA@DESKTOP-TBGHGCN.homenet.telecomitalia.it>
2018-05-08 11:16:42 +0200

    Messaggi della pagina risultati ricerca

--- Zacinto/src/main/java/zacintoView/RisultatiRicerca.java
+++ Zacinto/src/main/java/zacintoView/RisultatiRicerca.java
@@ -21,53 +21,59 @@ public class RisultatiRicerca extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		resp.setContentType("text/html"); 
 		String nextJSP ="/risultatiRicerca.jsp";
-		
+
 		Connection cn = null;
 		ResultSet rs = null;
-		
+
 		Prodotto rscerca = new Prodotto();
-		
+
 		String cerca = req.getParameter("cerca");
-		System.out.println(""+cerca);
+	
+		String prodotto = "Nessun prodotto corrisponde alla ricerca.";
+		
+		req.setAttribute("nomeProdotto", prodotto);
+		
 		//query
 		try {
 			cn = ConnessioneMysql.dbconnect();
 			rs = ConnessioneMysql.queryRs(cn, "SELECT * FROM `prodotti` WHERE nomeProdotto='"+ cerca +"'");
 			rs.first();
-			
+
 			//Setto i valori del db
 			rscerca.setId(rs.getInt("id"));
 			rscerca.setNomeProdotto(rs.getString("nomeProdotto"));
 			rscerca.setAutore(rs.getString("autore"));
 			rscerca.setEta(rs.getInt("eta"));
-			System.out.println(""+rscerca.getNomeProdotto());
+			
 			//Setto i valori degli attributi
 			req.setAttribute("nomeProdotto", rscerca.getNomeProdotto());
 			req.setAttribute("genere", rscerca.getGenere());
 			req.setAttribute("autore", rscerca.getAutore());
 			req.setAttribute("codice", rscerca.getId());
-			
+
+
+
+
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		
 		//nextJSP="/risultatiRicerca.jsp";
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
 		dispatcher.forward(req,resp);
 
-		
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- Zacinto/src/main/webapp/risultatiRicerca.jsp
+++ Zacinto/src/main/webapp/risultatiRicerca.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+	<%@ page import="zacintoController.Prodotto" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,6 +10,8 @@
 </head>
 <body>
 
+	<% Prodotto p = (Prodotto) request.getAttribute("nomeProdotto"); %>
+
 	<div class="logo">
 
 		<img src="img/BibliotecaZacintoTrasparente.png"
@@ -17,15 +20,20 @@
 			height="75px"> </img>
 
 	</div>
-	
+
 	<div class="box-errore">
 
-		
-		
-	
-		<form action="prodotto">
 
-			<button><%=request.getAttribute("textpulsante")%></button>
+
+
+		<form action="catalogo">
+
+			<% if(p != null) {%>
+			<p>
+				Prodotto trovato: <a href="prodotto"><%=request.getAttribute("nomeProdotto") %>
+			</p>
+				<% } %>
+			<button>Ritorna al catalogo</button>
 
 		</form>
 
